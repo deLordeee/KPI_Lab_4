80543a47a9db27f4d708ab030d677b4c47d9ee4f
Yevhenii Butkevych <143118683+deLordeee@users.noreply.github.com>
2025-05-24 20:20:28 +0300

    Update balancer_test.go
    Added initial test for setting up servers

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,28 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"sync/atomic"
+	"testing"
 
-func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте юніт-тест для балансувальника.
+	"github.com/stretchr/testify/assert"
+)
+
+func setupServers(count int) []*httptest.Server {
+	var servers []*httptest.Server
+	for i := 0; i < count; i++ {
+		si := i
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "/health" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+			
+			fmt.Fprintf(w, "server-%d", si)
+		}))
+		servers = append(servers, ts)
+	}
+	return servers
 }
