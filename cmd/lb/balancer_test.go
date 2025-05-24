3d1300db69c4a609b86bb5e1fb91622e47d550ad
Yevhenii Butkevych <143118683+deLordeee@users.noreply.github.com>
2025-05-24 20:26:22 +0300

    Update balancer_test.go
    Added more tests to check for incoming traffic

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -26,3 +26,53 @@ func setupServers(count int) []*httptest.Server {
 	}
 	return servers
 }
+
+func TestSelectBackendLeastTraffic(t *testing.T) {
+	
+	servers := setupServers(3)
+	defer func() {
+		for _, ts := range servers {
+			ts.Close()
+		}
+	}()
+
+
+	serversPool = []string{}
+	trafficCounters = make(map[string]*uint64)
+	for _, ts := range servers {
+		addr := ts.Listener.Addr().String()
+		serversPool = append(serversPool, addr)
+		trafficCounters[addr] = new(uint64)
+	}
+
+	
+	atomic.AddUint64(trafficCounters[serversPool[0]], 500)
+	atomic.AddUint64(trafficCounters[serversPool[1]], 100)
+	atomic.AddUint64(trafficCounters[serversPool[2]], 200)
+
+	
+	chosen := selectBackend()
+	assert.Equal(t, serversPool[1], chosen)
+}
+
+func TestForwardUpdatesTraffic(t *testing.T) {
+	
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "hello world")
+	}))
+	defer ts.Close()
+
+	addr := ts.Listener.Addr().String()
+	
+	serversPool = []string{addr}
+	trafficCounters = map[string]*uint64{addr: new(uint64)}
+
+	req := httptest.NewRequest("GET", "http://ignored", nil)
+	rw := httptest.NewRecorder()
+
+	err := forward(addr, rw, req)
+	assert.NoError(t, err)
+
+	cnt := atomic.LoadUint64(trafficCounters[addr])
+	assert.Equal(t, uint64(len("hello world")), cnt)
+}
