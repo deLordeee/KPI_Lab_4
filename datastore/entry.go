9d9cb29425e6ba5984caf73c76fc749ea37c3c73
Yevhenii Butkevych <143118683+deLordeee@users.noreply.github.com>
2025-05-25 21:19:07 +0300

    Update entry.go

--- datastore/entry.go
+++ datastore/entry.go
@@ -1,58 +1,46 @@
 package datastore
 
 import (
-	"bufio"
 	"encoding/binary"
-	"errors"
-	"fmt"
-	"io"
 )
 
 type entry struct {
-	key, value string
+	key       string
+	value     string
+	isDeleted bool
 }
 
-// 0           4    8     kl+8  kl+12     <-- offset
-// (full size) (kl) (key) (vl)  (value)
-// 4           4    ....  4     .....     <-- length
-
 func (e *entry) Encode() []byte {
-	kl, vl := len(e.key), len(e.value)
-	size := kl + vl + 12
-	res := make([]byte, size)
-	binary.LittleEndian.PutUint32(res, uint32(size))
-	binary.LittleEndian.PutUint32(res[4:], uint32(kl))
-	copy(res[8:], e.key)
-	binary.LittleEndian.PutUint32(res[kl+8:], uint32(vl))
-	copy(res[kl+12:], e.value)
-	return res
+	kl := len(e.key)
+	vl := len(e.value)
+	size := 4 + 1 + 4 + kl + 4 + vl
+	buf := make([]byte, size)
+	// total size
+	binary.LittleEndian.PutUint32(buf[0:4], uint32(size-4))
+	// flag
+	if e.isDeleted {
+		buf[4] = 1
+	} else {
+		buf[4] = 0
+	}
+	// key length
+	binary.LittleEndian.PutUint32(buf[5:9], uint32(kl))
+	copy(buf[9:9+kl], e.key)
+	// value length
+	off := 9 + kl
+	binary.LittleEndian.PutUint32(buf[off:off+4], uint32(vl))
+	copy(buf[off+4:off+4+vl], e.value)
+	return buf
 }
 
 func (e *entry) Decode(input []byte) {
-	e.key = decodeString(input[4:])
-	e.value = decodeString(input[len(e.key)+8:])
-}
-
-func decodeString(v []byte) string {
-	l := binary.LittleEndian.Uint32(v)
-	buf := make([]byte, l)
-	copy(buf, v[4:4+int(l)])
-	return string(buf)
-}
-
-func (e *entry) DecodeFromReader(in *bufio.Reader) (int, error) {
-	sizeBuf, err := in.Peek(4)
-	if err != nil {
-		if errors.Is(err, io.EOF) {
-			return 0, err
-		}
-		return 0, fmt.Errorf("DecodeFromReader, cannot read size: %w", err)
-	}
-	buf := make([]byte, int(binary.LittleEndian.Uint32(sizeBuf)))
-	n, err := in.Read(buf[:])
-	if err != nil {
-		return n, fmt.Errorf("DecodeFromReader, cannot read record: %w", err)
-	}
-	e.Decode(buf)
-	return n, nil
+	// flag
+	e.isDeleted = input[4] == 1
+	// key length
+	kl := int(binary.LittleEndian.Uint32(input[5:9]))
+	e.key = string(input[9 : 9+kl])
+	// value length
+	off := 9 + kl
+	vl := int(binary.LittleEndian.Uint32(input[off : off+4]))
+	e.value = string(input[off+4 : off+4+vl])
 }
