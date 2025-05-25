f16765b5f3f3400227050f9f324a55237e3257a5
Viacheslav Kolesnyk <155906971+ferer00@users.noreply.github.com>
2025-05-25 21:21:05 +0300

    Update db_test.go

--- datastore/db_test.go
+++ datastore/db_test.go
@@ -1,84 +1,34 @@
 package datastore
 
 import (
-	"testing"
+  "io/ioutil"
+  "os"
+
+  "testing"
 )
 
-func TestDb(t *testing.T) {
-	tmp := t.TempDir()
-	db, err := Open(tmp)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Cleanup(func() {
-		_ = db.Close()
-	})
+type Data struct { key, value string }
 
-	pairs := [][]string{
-		{"k1", "v1"},
-		{"k2", "v2"},
-		{"k3", "v3"},
-		{"k2", "v2.1"},
-	}
+func TestPutGetDelete(t *testing.T) {
+  dir, err := ioutil.TempDir("", "test-db")
+  if err != nil { t.Fatal(err) }
+  defer os.RemoveAll(dir)
 
-	t.Run("put/get", func(t *testing.T) {
-		for _, pair := range pairs {
-			err := db.Put(pair[0], pair[1])
-			if err != nil {
-				t.Errorf("Cannot put %s: %s", pairs[0], err)
-			}
-			value, err := db.Get(pair[0])
-			if err != nil {
-				t.Errorf("Cannot get %s: %s", pairs[0], err)
-			}
-			if value != pair[1] {
-				t.Errorf("Bad value returned expected %s, got %s", pair[1], value)
-			}
-		}
-	})
+  db, err := NewDb(dir, 100)
+  if err != nil { t.Fatal(err) }
+  defer db.Close()
 
-	t.Run("file growth", func(t *testing.T) {
-		sizeBefore, err := db.Size()
-		if err != nil {
-			t.Fatal(err)
-		}
-		for _, pair := range pairs {
-			err := db.Put(pair[0], pair[1])
-			if err != nil {
-				t.Errorf("Cannot put %s: %s", pairs[0], err)
-			}
-		}
-		sizeAfter, err := db.Size()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if sizeAfter <= sizeBefore {
-			t.Errorf("Size does not grow after put (before %d, after %d)", sizeBefore, sizeAfter)
-		}
-	})
-
-	t.Run("new db process", func(t *testing.T) {
-		if err := db.Close(); err != nil {
-			t.Fatal(err)
-		}
-		db, err = Open(tmp)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		uniquePairs := make(map[string]string)
-		for _, pair := range pairs {
-			uniquePairs[pair[0]] = pair[1]
-		}
-
-		for key, expectedValue := range uniquePairs {
-			value, err := db.Get(key)
-			if err != nil {
-				t.Errorf("Cannot get %s: %s", key, err)
-			}
-			if value != expectedValue {
-				t.Errorf("Get(%q) = %q, wanted %q", key, value, expectedValue)
-			}
-		}
-	})
+  data := []Data{{"k1", "v1"}, {"k2", "v2"}, {"k3", "v3"}}
+  for _, d := range data {
+    if err := db.Put(d.key, d.value); err != nil {
+      t.Errorf("Put error: %v", err)
+    }
+    if got, err := db.Get(d.key); err != nil || got != d.value {
+      t.Errorf("Get = %v, %v; want %v", got, err, d.value)
+    }
+  }
+  db.Delete("k2")
+  if _, err := db.Get("k2"); err != ErrNotFound {
+    t.Errorf("Deleted key found: %v", err)
+  }
 }
